e6cbabf52588d9a3f786fe3c57b238f6babb2f83
Armin Pech <github@arminpech.de>
2016-03-24 21:18:56 +0100

    tldns-a: fix mysql sld db init

--- dnssec-tldns-a/etc/mysql/init-db.sql
+++ dnssec-tldns-a/etc/mysql/init-db.sql
@@ -3,3 +3,5 @@ FLUSH PRIVILEGES;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 UPDATE user SET password=PASSWORD("root") WHERE user='root'
 CREATE DATABASE sld CHARSET utf8;
+USE sld;
+-- import data from sld below
