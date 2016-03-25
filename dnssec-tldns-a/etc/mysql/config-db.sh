51b2352b2f09109b94a01bcb5feb6f162d23708b
Armin Pech <github@arminpech.de>
2016-03-25 23:01:55 +0100

    Fix Setup script for mysql db

--- dnssec-tldns-a/etc/mysql/config-db.sh
+++ dnssec-tldns-a/etc/mysql/config-db.sh
@@ -4,10 +4,16 @@ set -e
 
 sed -i 's/password = .*/password = root/g' /etc/mysql/debian.cnf
 sed -i 's/^log_error/# log_error/' /etc/mysql/mysql.conf.d/mysqld.cnf
+cat > /etc/mysql/mysql.conf.d/mysql-skip-name-resolv.cnf <<EOF
+[mysqld]
+skip_name_resolve
+EOF
+
 mysql_install_db --user=mysql
 mysqld_safe &
-sleep 30
-mysql -uroot -e "GRANT ALL PRIVILEGES on *.* TO 'debian-sys-maint'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;"
+sleep 60
+
+mysql -uroot -proot -e "GRANT ALL PRIVILEGES on *.* TO 'debian-sys-maint'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;"
 
 mysql --defaults-file=/etc/mysql/debian.cnf -e "CREATE DATABASE sld CHARSET utf8;"
 mysql --defaults-file=/etc/mysql/debian.cnf < /etc/whoisd/sld.sql
