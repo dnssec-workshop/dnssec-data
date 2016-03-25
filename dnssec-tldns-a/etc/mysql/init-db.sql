ad3124e1b0b1a1e5a0af88bd4aead0f7c3fc5883
Armin Pech <github@arminpech.de>
2016-03-25 21:26:14 +0100

    dnssec-tldns-a: configure debian-sys-maintainer

--- dnssec-tldns-a/etc/mysql/init-db.sql
+++ dnssec-tldns-a/etc/mysql/init-db.sql
@@ -1,7 +1,11 @@
 USE mysql;
 FLUSH PRIVILEGES;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
-UPDATE user SET password=PASSWORD("root") WHERE user='root'
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION;
+
+-- Creating debian-sys-maint user
+GRANT ALL PRIVILEGES on *.* TO 'debian-sys-maint'@'localhost' IDENTIFIED BY '' WITH GRANT OPTION;
+
+-- create database for domains
 CREATE DATABASE sld CHARSET utf8;
 USE sld;
 -- import data from sld below
