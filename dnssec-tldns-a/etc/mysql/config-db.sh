6c47929d801e487d9aec0654a2ebe905c5d1b238
Armin Pech <github@arminpech.de>
2016-03-26 12:16:19 +0100

    Fix mysql db init

--- dnssec-tldns-a/etc/mysql/config-db.sh
+++ dnssec-tldns-a/etc/mysql/config-db.sh
@@ -2,7 +2,7 @@
 set -x
 set -e
 
-sed -i 's/password = .*/password =/g' /etc/mysql/debian.cnf
+sed -i 's/password = .*/password =/' /etc/mysql/debian.cnf
 sed -i 's/^log_error/# log_error/' /etc/mysql/mysql.conf.d/mysqld.cnf
 cat > /etc/mysql/mysql.conf.d/mysql-skip-name-resolv.cnf <<EOF
 [mysqld]
@@ -10,13 +10,13 @@ skip_name_resolve
 EOF
 
 mysql_install_db --user=mysql
-mysqld_safe &
-sleep 60
 
+cd /usr ; mysqld &
+sleep 15
+mysqladmin -u root status
 mysql -uroot -e "GRANT ALL PRIVILEGES on *.* TO 'debian-sys-maint'@'localhost' IDENTIFIED BY '' WITH GRANT OPTION;"
-
 mysql --defaults-file=/etc/mysql/debian.cnf -e "GRANT ALL PRIVILEGES ON *.* TO root@localhost IDENTIFIED BY 'root'"
+mysql --defaults-file=/etc/mysql/debian.cnf -e "GRANT ALL PRIVILEGES ON *.* TO root@127.0.0.1 IDENTIFIED BY 'root'"
 mysql --defaults-file=/etc/mysql/debian.cnf -e "CREATE DATABASE sld CHARSET utf8;"
-mysql --defaults-file=/etc/mysql/debian.cnf < /etc/whoisd/sld.sql
-
+mysql --defaults-file=/etc/mysql/debian.cnf sld < /etc/whoisd/sld.sql
 mysqladmin --defaults-file=/etc/mysql/debian.cnf shutdown
