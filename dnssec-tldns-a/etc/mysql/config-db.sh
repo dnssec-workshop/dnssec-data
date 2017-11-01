4e7d39a891539a89d042b584f349187e709670b3
Armin Pech <a.pech@babiel.com>
2017-11-01 19:13:14 +0100

    tldns-a: config-db.sh mysql root login

--- dnssec-tldns-a/etc/mysql/config-db.sh
+++ dnssec-tldns-a/etc/mysql/config-db.sh
@@ -2,18 +2,16 @@
 set -x
 set -e
 
-sed -i 's/password = .*/password =/' /etc/mysql/debian.cnf
+sed -i 's/password = .*/password = debian/' /etc/mysql/debian.cnf
 sed -i 's/^log_error/# log_error/' /etc/mysql/mysql.conf.d/mysqld.cnf
 cat > /etc/mysql/mysql.conf.d/mysql-skip-name-resolv.cnf <<EOF
 [mysqld]
 skip_name_resolve
 EOF
 
-mysqld --initialize --user=mysql --datadir /var/lib/mysql
-
 cd /usr ; mysqld &
 sleep 15
-mysql -uroot -proot -e "GRANT ALL PRIVILEGES on *.* TO 'debian-sys-maint'@'localhost' IDENTIFIED BY '' WITH GRANT OPTION;"
+mysql -uroot -proot -e "GRANT ALL PRIVILEGES on *.* TO 'debian-sys-maint'@'localhost' IDENTIFIED BY 'debian' WITH GRANT OPTION;"
 mysql --defaults-file=/etc/mysql/debian.cnf -e "GRANT ALL PRIVILEGES ON *.* TO root@localhost IDENTIFIED BY 'root'"
 mysql --defaults-file=/etc/mysql/debian.cnf -e "GRANT ALL PRIVILEGES ON *.* TO root@127.0.0.1 IDENTIFIED BY 'root'"
 mysql --defaults-file=/etc/mysql/debian.cnf -e "CREATE DATABASE sld CHARSET utf8;"
